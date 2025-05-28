16ffcd634a013eb60eccd910ce264b17b68dabaf
Soucius <soucius.ggwp@gmail.com>
2025-05-28 13:40:46 +0300

    optimizing file and folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,9 @@
 import express from "express";
+import notesRoutes from "./src/routes/notes.routes.js";
 
 const app = express();
 
-app.get("/api/notes", (req, res) => {
-    res.status(200).send("You got 5 notes");
-});
-
-app.post("/api/notes", (req, res) => {
-    res.status(201).json({ message: "Note created successfully!" });
-});
-
-app.put("/api/notes/:id", (req, res) => {
-    res.status(200).json({ message: "Note updated successfully!" });
-});
-
-app.delete("/api/notes/:id", (req, res) => {
-    res.status(200).json({ message: "Note deleted successfully!" });
-});
+app.use("/api/notes", notesRoutes);
 
 app.listen(5001, () => {
     console.log("Server started on: http://localhost:5001");
