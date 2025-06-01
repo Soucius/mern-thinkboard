bb5a53114ea1fe0e9f68fe09ee65db2aa7b94df9
Soucius <soucius.ggwp@gmail.com>
2025-06-01 15:06:19 +0300

    setting up database

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import notesRoutes from "./routes/notes.routes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use("/api/notes", notesRoutes);
+
+connectDB();
+
+app.listen(PORT, () => {
+    console.log("Server started on: http://localhost:5001");
+});
